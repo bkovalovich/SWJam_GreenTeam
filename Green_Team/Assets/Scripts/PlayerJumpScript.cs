c00b460f3a8736f637ffa79db5bf04b888a12232
Hyungster <131295827+Hyungster@users.noreply.github.com>
2023-04-22 15:58:53 -0400

    Add files via upload

--- Green_Team/Assets/Scripts/PlayerJumpScript.cs
+++ Green_Team/Assets/Scripts/PlayerJumpScript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-public class PlayerJumpScript : MonoBehaviour {
+public class PlayerJumpScript : MonoBehaviour
+{
     private Rigidbody2D rb;
     public PlayerInputActions playerControls;
     private InputAction jump;
@@ -15,20 +16,25 @@ public class PlayerJumpScript : MonoBehaviour {
     public Animator anim;
 
 
-    private void Awake() {
+    private void Awake()
+    {
         rb = GetComponentInParent<Rigidbody2D>();
         playerControls = new PlayerInputActions();
     }
-    private void OnEnable() {
+    private void OnEnable()
+    {
         jump = playerControls.Player.Jump;
         jump.Enable();
         jump.performed += Jump;
     }
-    private void OnDisable() {
+    private void OnDisable()
+    {
         jump.Disable();
     }
-    private void Jump(InputAction.CallbackContext context) {
-        if (canJump) {
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (canJump)
+        {
             rb.AddForce(Vector3.up * jumpSpeed);
             canJump = false;
 
@@ -36,7 +42,12 @@ public class PlayerJumpScript : MonoBehaviour {
             anim.Play("Player_Jump", -1, 0f);
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionStay2D(Collision2D collision)
+    {
         canJump = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        canJump = false;
     }
 }
