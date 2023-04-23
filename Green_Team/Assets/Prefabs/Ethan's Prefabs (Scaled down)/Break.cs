23e25d6f32d955b7ad105db111b2e28a64a535a9
et402 <76020248+et402@users.noreply.github.com>
2023-04-23 10:35:19 -0400

    Add files via upload

---
+++ Green_Team/Assets/Prefabs/Ethan's Prefabs (Scaled down)/Break.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break : MonoBehaviour
+{
+    public Transform origin;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.AddForce((transform.position - origin.position)*100);
+        rb.AddTorque(Random.Range(-100, 100));
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
