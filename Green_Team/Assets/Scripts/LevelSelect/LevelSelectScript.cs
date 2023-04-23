9841181eb0c93f5b5676a5f556502b7005a69998
bkova <dampnoodle123@gmail.com>
2023-04-22 20:49:33 -0400

    Misc Changes

--- Green_Team/Assets/Scripts/LevelSelect/LevelSelectScript.cs
+++ Green_Team/Assets/Scripts/LevelSelect/LevelSelectScript.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelectScript : MonoBehaviour
 {
     [SerializeField] List<string> levels = new List<string>();
-    // Start is called before the first frame update
+    private List<Button> buttons = new List<Button>(); 
     void Start()
     {
 
